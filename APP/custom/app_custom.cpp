80a9cc79a98671eb2bcc035d5721fe46ea9b6f04
gyj0415 <3494872043@qq.com>
2025-11-07 17:17:57 +0800

    fix app_custom,修正代码格式

--- APP/custom/app_custom.cpp
+++ APP/custom/app_custom.cpp
@@ -40,6 +40,7 @@ Motor::DMMotor DM_Motor3("Motor3",Motor::DMMotor::J4310,{
 
 delta::Kinematics my_delta(100,40,100,224);
 delta::Dynamic my_dynamic(100,40,100,224);
+// Controller::PID pid_x, pid_y, pid_z;
 
 // 静态任务，在 CubeMX 中配置
 void app_custom_task(void *args) {
@@ -48,39 +49,54 @@ void app_custom_task(void *args) {
 
     OS::Task::SleepMilliseconds(3000);
     float pos[3];
-    float rpy[3];
-    float tor[3];
+    // float rpy[3];
+    float tor[3] = {0, 0, 0};;
     float deg[3];
-    float deg_test[3];
     float target_force[3] = {0, 0, 24};
+    float deg_t[3];
+    float pos_t[3] = {29, -22, -220};
+    // pid_x.set_para(0.005, 0, 1, 1, 0);
+    // pid_y.set_para(0.005, 0, 1, 1, 0);
+    // pid_z.set_para(0.005, 0, 1, 1, 0);
 
     while(true) {
 
-        DM_Motor1.control(0,0,0,0,tor[0]);
-        DM_Motor2.control(0,0,0,0,tor[1]);
-        DM_Motor3.control(0,0,0,0,tor[2]);
+        DM_Motor1.control(deg_t[0],1,1,1,tor[0]);
+        DM_Motor2.control(deg_t[1],1,1,1,tor[1]);
+        DM_Motor3.control(deg_t[2],1,1,1,tor[2]);
         deg[0] = -DM_Motor1.status.pos;
         deg[1] = -DM_Motor2.status.pos;
         deg[2] = -DM_Motor3.status.pos;
-        my_delta.delta_forward_clc(deg[0],deg[1],deg[2],pos);
-        my_dynamic.tor_clc(deg[0],deg[1],deg[2],pos,target_force[0],target_force[1],target_force[2],tor);
 
-        my_delta.delta_inverse_clc(pos, deg_test);
+        Matrixf<3, 1> deg_(deg);
+        Matrixf<3, 1> t_force(target_force);
+        Matrixf<3, 1> t_pos(pos_t);
+        Matrixf<3, 1> pos_ = my_delta.delta_forward_clc(deg_);
+        Matrixf<3, 1> tor_ = my_dynamic.tor_clc(deg_, pos_, t_force);
+        Matrixf<3, 1> t_deg = my_delta.delta_inverse_clc(t_pos);
+
+        for(uint8_t i = 0; i < 3; i++) {
+            pos[i] = pos_[i][0];
+            tor[i] = tor_[i][0];
+            deg_t[i] = t_deg[i][0];
+        }
 
         app_msg_vofa_send(E_UART_DEBUG,
             // rpy[0],
             // rpy[1],
             // rpy[2],
-            // pos[0],
-            // pos[1],
-            // pos[2],
+            pos[0],
+            pos[1],
+            pos[2],
             1.0*tor[0],
             1.0*tor[1],
-            1.0*tor[2],
-            DM_Motor2.status.torque,
-            DM_Motor1.status.pos,
-            DM_Motor2.status.pos,
-            DM_Motor3.status.pos
+            1.0*tor[2]
+            // pid_x.update(deg[0], deg_t[0]),
+            // pid_y.update(deg[1], deg_t[1]),
+            // pid_z.update(deg[2], deg_t[2])
+            // DM_Motor1.status.pos,
+            // DM_Motor2.status.pos,
+            // DM_Motor3.status.pos
             );
         OS::Task::SleepMilliseconds(1);
     }
--- APP/custom/app_custom.h
+++ APP/custom/app_custom.h
@@ -28,33 +28,25 @@ namespace delta {
         public:
         Kinematics();
         Kinematics(float const R_, float const r_, float const L_, float const l_) : R(R_), r(r_), L(L_), l(l_) {
-            theta_1 = theta_2 = theta_3 = 0;
-            float data[3] = {R_,0,0};
-            Matrixf<3,1> temp(data);
-            pos_oc1 =  temp.rot_z(phi[0]) * temp;
-            pos_oc2 =  temp.rot_z(phi[1]) * temp;
-            pos_oc3 =  temp.rot_z(phi[2]) * temp;
-            data[0] = -r_;
-            Matrixf<3,1> temp2(data);
-            pos_a1p =  temp.rot_z(phi[0]) * temp2;
-            pos_a2p =  temp.rot_z(phi[1]) * temp2;
-            pos_a3p =  temp.rot_z(phi[2]) * temp2;
+            float data1[3] = {R_,0,0}, data2[3] = {-r_, 0, 0};
+            Matrixf<3,1> temp1(data1), temp2(data2);
+            for(uint8_t i = 0; i < 3; i++) {
+                pos_oc[i] = temp1.rot_z(phi[i]) * temp1;
+                pos_ap[i] = temp2.rot_z(phi[i]) * temp2;
+                theta_i[i] = 0;
+            }
         };
 
-        //将来想改为Matrixf<3,1>类型
-        void delta_forward_clc(float _theta1, float _theta2, float _theta3, float *position) {
-            theta_1 = _theta1 ,theta_2 = _theta2 ,theta_3 = _theta3;
-            get_cbi(theta_1, phi[0], &pos_cb1);
-            get_cbi(theta_2, phi[1], &pos_cb2);
-            get_cbi(theta_3, phi[2], &pos_cb3);
+        Matrixf<3,1> delta_forward_clc(const Matrixf<3, 1>& _theta) {
+            theta_i[0] = _theta[0][0] ,theta_i[1] = _theta[1][0] ,theta_i[2] = _theta[2][0];
+            for(uint8_t i = 0; i < 3; i++) {
+                get_cbi(theta_i[i], phi[i], &pos_cb[i]);
+                pos_od[i] = pos_oc[i] + pos_cb[i] + pos_ap[i];
+            }
 
-            pos_od1 = pos_oc1 + pos_cb1 + pos_a1p;
-            pos_od2 = pos_oc2 + pos_cb2 + pos_a2p;
-            pos_od3 = pos_oc3 + pos_cb3 + pos_a3p;
-
-            vector_d12 = pos_od2 - pos_od1;
-            vector_d23 = pos_od3 - pos_od2;
-            vector_d13 = pos_od3 - pos_od1;
+            vector_d12 = pos_od[1] - pos_od[0];
+            vector_d23 = pos_od[2] - pos_od[1];
+            vector_d13 = pos_od[2] - pos_od[0];
 
             vector_t.vector_cross_multiple(vector_d13,vector_d12);
             vector_t /= vector_t.norm();
@@ -70,14 +62,14 @@ namespace delta {
             float R_D = a*b*c/(4*s);
             Matrixf<3,1> vector_ef = sqrt(R_D*R_D - c*c/4)*vector_n;
             Matrixf<3,1> vector_fp = sqrt(l*l - R_D*R_D)*vector_t;
-            pos_op = (pos_od2+pos_od3)/2+vector_ef+vector_fp;
-            position[0] = pos_op[0][0];
-            position[1] = pos_op[1][0];
-            position[2] = pos_op[2][0];
+            pos_op = (pos_od[1]+pos_od[2])/2+vector_ef+vector_fp;
+
+            return pos_op;
         }
 
-        void delta_inverse_clc(float *pos, float *theta) {
-            float A[3] = {}, B[3] = {}, C[3] = {}, t[3] = {};
+        Matrixf<3, 1> delta_inverse_clc(const Matrixf<3, 1>& _pos) {
+            float pos[3] = {_pos[0][0], _pos[1][0], _pos[2][0]};
+            float A[3] = {}, B[3] = {}, C[3] = {}, t[3] = {}, theta[3] = {};
             float temp = (R-r)*(R-r) + pos[0]*pos[0] + pos[1]*pos[1] + pos[2]*pos[2] + L*L - l*l;
             for(uint8_t i = 0; i<3; i++) {
                 A[i] = temp - 2*pos[0]*cos(phi[i])*(R-r) - 2*pos[1]*sin(phi[i])*(R-r) - 2*(R-r)*L + 2*pos[0]*cos(phi[i])*L + 2*pos[1]*sin(phi[i])*L;
@@ -86,11 +78,12 @@ namespace delta {
                 t[i] = (-B[i] - sqrt(B[i]*B[i] - 4*A[i]*C[i])) / (2*A[i]);
                 theta[i] = 2.0f * atan2(t[i], 1.0f);
             }
+            return {theta};
         }
 
     private:
         void get_cbi(float _theta, float _phi, Matrixf<3,1> *mat) {
-            float data[3] = {L*cos(_theta),0,-L*sin(_theta)};
+            float data[3] = {L*cos(_theta),0, -L*sin(_theta)};
             Matrixf<3,1> temp(data);
             *mat = temp.rot_z(_phi) * temp;
         }
@@ -98,11 +91,11 @@ namespace delta {
         const float phi[3] = {0,
                         2.0f * M_PI / 3.0f,
                         4.0f * M_PI / 3.0f,};
-        float theta_1,theta_2,theta_3;
-        Matrixf<3,1> pos_oc1, pos_oc2, pos_oc3;
-        Matrixf<3,1> pos_cb1, pos_cb2, pos_cb3;
-        Matrixf<3,1> pos_a1p, pos_a2p, pos_a3p;
-        Matrixf<3,1> pos_od1, pos_od2, pos_od3;
+        float theta_i[3];
+        Matrixf<3,1> pos_oc[3];
+        Matrixf<3,1> pos_cb[3];
+        Matrixf<3,1> pos_ap[3];
+        Matrixf<3,1> pos_od[3];
         Matrixf<3,1> vector_d13, vector_d12, vector_d23;
         Matrixf<3,1> vector_t, vector_n;
         Matrixf<3,1> pos_op;
@@ -111,76 +104,62 @@ namespace delta {
         public:
         Dynamic();
         Dynamic(float const R_, float const r_, float const L_, float const l_): R(R_), r(r_), L(L_), l(l_) {
-            phi_1 = 0;
-            phi_2 = PI*2/3;
-            phi_3 = PI*4/3;
-            theta_1 = theta_2 = theta_3 = 0;
-            float data[3] = {R_,0,0};
-            Matrixf<3,1> temp(data);
-            pos_oc1 =  temp.rot_z(phi_1) * temp;
-            pos_oc2 =  temp.rot_z(phi_2) * temp;
-            pos_oc3 =  temp.rot_z(phi_3) * temp;
-            data[0] = r_;
-            Matrixf<3,1> temp2(data);
-            pos_pa1 =  temp.rot_z(phi_1) * temp2;
-            pos_pa2 =  temp.rot_z(phi_2) * temp2;
-            pos_pa3 =  temp.rot_z(phi_3) * temp2;
+            float data1[3] = {R_,0,0}, data2[3] = {r_, 0, 0};
+            Matrixf<3,1> temp1(data1), temp2(data2);
+            for(uint8_t i = 0; i < 3; i++) {
+                pos_oc[i] = temp1.rot_z(phi[i]) * temp1;
+                pos_pa[i] = temp2.rot_z(phi[i]) * temp2;
+                theta_i[i] = 0;
+            }
         }
 
-        //打算换成Matrixf<3,1>
-        void tor_clc(float _theta1, float _theta2, float _theta3, float *pos, float force_x, float force_y, float force_z, float *answer) {
-            theta_1 = (_theta1), theta_2 = (_theta2), theta_3 = (_theta3);
-            Matrixf<3,1> vector_OP(pos);
-            get_si(phi_1,theta_1,&pos_b1,&pos_cb1,&pos_s1,pos_pa1,pos_oc1,vector_OP);
-            get_si(phi_2,theta_2,&pos_b2,&pos_cb2,&pos_s2,pos_pa2,pos_oc2,vector_OP);
-            get_si(phi_3,theta_3,&pos_b3,&pos_cb3,&pos_s3,pos_pa3,pos_oc3,vector_OP);
-            Matrixf<1,3> t_s1 = pos_s1.trans();
-            Matrixf<1,3> t_s2 = pos_s2.trans();
-            Matrixf<1,3> t_s3 = pos_s3.trans();
-            Matrixf<3,3> temp1 = matrixf::vertcat(matrixf::vertcat(t_s1, t_s2), t_s3);
+        Matrixf<3, 1>  tor_clc(const Matrixf<3, 1>& _theta, const Matrixf<3, 1>& vector_OP, const Matrixf<3, 1>& force) {
+            Matrixf<3, 1> vec_op = vector_OP, vec_force = force;
+            theta_i[0] = _theta[0][0] ,theta_i[1] = _theta[1][0] ,theta_i[2] = _theta[2][0];
+            Matrixf<1,3> t_s[3];
+            for(uint8_t i = 0; i < 3; i++) {
+                get_bi(theta_i[i],phi[i],&pos_b[i]);
+                get_cbi(theta_i[i],phi[i],&pos_cb[i]);
+                pos_s[i] = (vec_op + pos_pa[i] - pos_oc[i] - pos_cb[i])/1000;
+                t_s[i] = pos_s[i].trans();
+            }
+
+            Matrixf<3,3> temp1 = matrixf::vertcat(matrixf::vertcat(t_s[0], t_s[1]), t_s[2]);
             temp1 = matrixf::inv(temp1);
-            Matrixf<1,1> temp;
             Matrixf<3,3> temp2 = matrixf::zeros<3,3>();
-            temp = t_s1*pos_b1/1000;
-            temp2[0][0] = temp[0][0];
-            temp = t_s2*pos_b2/1000;
-            temp2[1][1] = temp[0][0];
-            temp = t_s3*pos_b3/1000;
-            temp2[2][2] = temp[0][0];
+            for(uint8_t i = 0; i < 3; i++) {
+                Matrixf<1,1> temp = t_s[i] * pos_b[i] / 1000;
+                temp2[i][i] = temp[0][0];
+            }
             Matrixf<3,3> jacobi = (temp1)*(temp2);
-            float force[3] = {force_x,force_y,force_z};
-            Matrixf<1,3> f(force);
+            Matrixf<1,3> f = vec_force.trans();
             tor = f*jacobi;
-            answer[0] = tor[0][0];
-            answer[1] = tor[0][1];
-            answer[2] = tor[0][2];
+
+            return tor.trans();
         }
 
     private:
-        void get_bi(float _theta, float _phi, Matrixf<3,1> *mat) {
+        void get_bi(float _theta, float _phi, Matrixf<3,1> *mat) const {
             //这里正负号存疑
             float data[3] = {L*sin(_theta),0,L*cos(_theta)};
             Matrixf<3,1> temp(data);
             *mat = temp.rot_z(_phi) * temp;
         }
-        void get_cbi(float _theta, float _phi, Matrixf<3,1> *mat) {
+        void get_cbi(float _theta, float _phi, Matrixf<3,1> *mat) const {
             float data[3] = {L*cos(_theta),0,-L*sin(_theta)};
             Matrixf<3,1> temp(data);
             *mat = temp.rot_z(_phi) * temp;
         }
-        void get_si( float _phi, float _theta, Matrixf<3,1> *b,Matrixf<3,1> *cb, Matrixf<3,1> *si, Matrixf<3,1> pa, Matrixf<3,1> oc,Matrixf<3,1> op) {
-            get_bi(_theta,_phi,b);
-            get_cbi(_theta,_phi,cb);
-            *si = (op + pa - oc - *cb)/1000;
-        }
         float R,r,L,l;
-        float phi_1,phi_2,phi_3;
-        float theta_1,theta_2,theta_3;
-        Matrixf<3,1> pos_oc1, pos_oc2, pos_oc3;
-        Matrixf<3,1> pos_b1, pos_b2, pos_b3;
-        Matrixf<3,1> pos_cb1, pos_cb2, pos_cb3;
-        Matrixf<3,1> pos_pa1, pos_pa2, pos_pa3;
-        Matrixf<3,1> pos_s1, pos_s2, pos_s3;
+        const float phi[3] = {0,
+                        2.0f * M_PI / 3.0f,
+                        4.0f * M_PI / 3.0f,};
+        float theta_i[3];
+        Matrixf<3,1> pos_oc[3];
+        Matrixf<3,1> pos_b[3];          //x+bθ其中的b
+        Matrixf<3,1> pos_cb[3];
+        Matrixf<3,1> pos_pa[3];
+        Matrixf<3,1> pos_s[3];
         Matrixf<1,3> tor;
     };
 }
